1fb00abb93d26294dd4d45feda65ae10a094bec8
TumpiPro <118016898+TumpiPro@users.noreply.github.com>
2024-04-16 19:14:33 +0200

    Add files via upload

---
+++ program.js
@@ -0,0 +1,25 @@
+console.log("Hello Majmok")
+
+alert("Vigyázat Négerek")
+
+let név = "Buznyák Péter"
+
+let bemenet = document.getElementById("bemenet")
+let ertek = bemenet.value
+console.log(ertek)
+bemenet.value = ""
+
+document.body.style.backgroundColor = "Pink"
+
+let gomb = document.getElementById("Kattgom")
+function katt(){
+    console.log("Gombra katt")
+    let randomColor = Math.floor(Math.random() * 1677722).toString(16)
+    document.body.style.backgroundColor = "#"+randomColor
+    window.open("http://www.pornhub.com", "_blank")
+
+    let ujszüveg = document.createElement("h1")
+    ujszüveg.innerText = "Rákattintottál"
+    document.body.appendChild(ujszüveg)
+}
+gomb.addEventListener("click", katt)
