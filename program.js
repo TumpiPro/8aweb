240083bb5c1a2c7f198c95fa24311df4d1d16baa
TumpiPro <118016898+TumpiPro@users.noreply.github.com>
2024-04-20 08:25:02 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-console.log("Hello Majmok")
+
 
 alert("Figyelem ez az oldal egy poén az osztájtársaim részére!!!(HA TE NEM VAGY ŐK HAGYD EL AZ OLDALT)")
 
@@ -34,3 +34,30 @@ function katt(){
     document.body.appendChild(ujszüveg)
 }
 gomb.addEventListener("click", katt)
+
+(function(console){
+
+console.save = function(data, Kártya.txt){
+
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if(!Kártya.txt) Kártya.txt= 'console.json'
+
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = Kártya.txt
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console) 
